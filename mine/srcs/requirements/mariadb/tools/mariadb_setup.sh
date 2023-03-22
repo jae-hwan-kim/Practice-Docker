6285439157c294f2e73e4d04456dff252dde659a
jae-hwan-kim <woghks7859@gmail.com>
2023-03-22 03:43:55 -0400

    CHORE: 주석 제거

--- mine/srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ mine/srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,25 +1,5 @@
 #!/bin/sh
 
-# service mysql start
-# 	echo "0"
-
-# if [ ! -e /var/lib/mysql/mariadb ]; then
-# 	echo "1"
-# 	mysql -u root -e "CREATE DATABASE IF NOT EXISTS mariadb" -p1234
-# 	echo "2"
-# 	mysql -u root -e "CREATE USER IF NOT EXISTS 'jaekim'@'%' IDENTIFIED BY '1234'" -p1234
-# 	echo "3"
-# 	mysql -u root -e "GRANT ALL PRIVILEGES ON mariadb.* TO 'jaekim'@'%'; FLUSH PRIVILEGES;" -p1234
-# 	echo "4"
-# 	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '5678'" -p1234
-# 	echo "5"
-# 	# mysqladmin -uroot -p1234 shutdown -p1234
-# 	pkill mariadb
-# fi
-# 	echo "6"
-
-# exec "mysqld_safe"
-
 service mysql start
 if [ ! -e /var/lib/mysql/$MYSQL_DATABASE ]; then
 	mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE" -p$MYSQL_PASSWORD
